797a6caf42df8ba59ccb7c47f89011d6037d1317
Sam Gladstone <gladston@umich.edu>
2017-09-24 22:11:26 -0400

    Cleanup: Syntactically sweetly object-oriented ;)

--- background.js
+++ background.js
@@ -1,10 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.sendMessage(tab.id, { message: 'GET CL INFO' }); 
 });
-
-// chrome.runtime.onMessage.addListener((response, sender, sendResponse) => {
-//   if (response.message === 'CL POST INFO') {
-//     // TODO: Send message to content?
-//     // alert(assembleSpreadsheetRowText(response.craigslistPostInfo));
-//   }
-// });
--- content.js
+++ content.js
@@ -1,120 +1,128 @@
-function getBedBathArray(firstRow) {
-  const bedsAndBaths = firstRow[0];
-  const bedBathArray = firstRow[0].textContent.split(' / ');
-  if (!bedBathArray || !bedBathArray.length || bedBathArray.length !== 2) {
-    const errorMessage = 'Ya dun goofed - something went wrong while trying to get bedroom and bathroom count :(';
-    alert(errorMessage);
-    throw new Error(errorMessage);
+class CraigslistPostInfo {
+  constructor() {
+    const firstRow = this.getFirstRow();
+    const bedBathArray = this.getBedBathArray(firstRow);
+    this.getNumBedrooms(bedBathArray);
+    this.getNumBathrooms(bedBathArray);
+    this.getAvailableDate(firstRow);
+    this.getSecondRowInfo();
+    this.getAddress();
+    this.getTitleInfo();
+    this.getUrl();
+    this.getCraigslistPostId();
   }
-  return bedBathArray;
-}
 
-function getNumBedrooms(firstRow) {
-  const bedBathArray = getBedBathArray(firstRow);
-  const bedroomsString = bedBathArray[0];
-  const bedRegex = /\d+BR/i;
-  if (bedRegex.test(bedBathArray[0])) { // TODO: Make this clearer?
-    return parseInt(bedBathArray[0].match(/\d+/)[0]);
+  getFirstRow() {
+    return document.getElementsByClassName('shared-line-bubble');
   }
-}
 
-function getNumBathrooms(firstRow) {
-  const bedsAndBaths = firstRow[0];
-  const bedBathArray = getBedBathArray(firstRow);
-  const bedroomsString = bedBathArray[0];
-  const bathRegex = /\d+BA/i;
-  if (bathRegex.test(bedBathArray[1])) { // TODO: Make this clearer?
-    return parseInt(bedBathArray[1].match(/\d+/)[0]);
-  }
-}
-
-function getAvailableDate(firstRow) {
-  if (!(firstRow && firstRow.length && firstRow.length >= 3 && firstRow[2])) {
-    console.log('No available date listed - skipping');
-    return;
-  }
-  const availableDateFullString = firstRow[2].textContent;
-  return availableDateFullString.split('available ')[1]; // E.g. 'oct 1'
-}
-
-function getSecondRowInfo(craigslistPostInfo) {
-  const sections = [].slice.call(document.getElementsByClassName('attrgroup'));
-  sections.forEach(section => {
-    const spans = [].slice.call(section.children).filter(el => el.tagName === 'SPAN');
-    spans.forEach(span => {
-      if (/parking/i.test(span.innerText) || /garage/i.test(span.innerText) || /carport/i.test(span.innerText)) {
-        craigslistPostInfo.parking = span.innerText;
-      }
-      if (/house/i.test(span.innerText) || /apartment/i.test(span.innerText) || /condo/i.test(span.innerText) || /duplex/i.test(span.innerText)) {
-        craigslistPostInfo.housingType = span.innerText;
-      }
-      if (/w\/d/i.test(span.innerText) || /laundry/i.test(span.innerText)) {
-        craigslistPostInfo.washerDryer = span.innerText;
-      }
-      if (/furnished/i.test(span.innerText)) {
-        craigslistPostInfo.furnished = 'Yes';
-      }
-    });
-  });
-  return craigslistPostInfo;
-}
-
-function getTitleInfo(craigslistPostInfo) {
-  const titleArray = document.querySelectorAll('h2.postingtitle');
-  if (titleArray.length && titleArray.length !== 1) {
-    const errorMessage = 'Something went wrong while trying to get the title :(';
-    alert(errorMessage);
-    throw new Error(errorMessage);
-  }
-  const title = titleArray[0];
-  // TODO: Add some validation/checks here
-  const titleSpan = [].slice.call(document.querySelectorAll('h2.postingtitle')[0].children).filter(child => child.className === 'postingtitletext')[0];
-  const titleChildren = [].slice.call(titleSpan.children);
-  craigslistPostInfo.price = parseInt(titleChildren.filter(child => child.className === 'price')[0].innerText.replace('$',''));
-  // TODO: validation
-  craigslistPostInfo.squareFootage = document.querySelectorAll('span.housing')[0].innerText.match(/\d+ft2/i)[0].match(/\d+/)[0];
-  craigslistPostInfo.title = document.getElementById('titletextonly').innerText;
-  return craigslistPostInfo;
-}
-
-function getAddress(craigslistPostInfo) {
-  const addressArray = document.querySelectorAll('div.mapaddress');
-  if (addressArray && addressArray.length && addressArray.length === 1) {
-    craigslistPostInfo.address = addressArray[0].innerText;
-  }
-  return craigslistPostInfo;
-}
-
-function getAllCraigslistPostInfo() {
-  const firstRow = document.getElementsByClassName('shared-line-bubble');
-  let craigslistPostInfo = {};
-  if (firstRow.length) {
-    if (firstRow.length > 0) {
-      craigslistPostInfo = {
-        numBedrooms: getNumBedrooms(firstRow),
-        numBathrooms: getNumBathrooms(firstRow),
-        availableDate: getAvailableDate(firstRow)
-      };
+  getBedBathArray(firstRow) {
+    const bedsAndBaths = firstRow[0];
+    const bedBathArray = firstRow[0].textContent.split(' / ');
+    if (!bedBathArray || !bedBathArray.length || bedBathArray.length !== 2) {
+      const errorMessage = 'Ya dun goofed - something went wrong while trying to get bedroom and bathroom count :(';
+      alert(errorMessage);
+      throw new Error(errorMessage);
     }
-    craigslistPostInfo = getSecondRowInfo(craigslistPostInfo);
-    craigslistPostInfo = getTitleInfo(craigslistPostInfo);
-    craigslistPostInfo = getAddress(craigslistPostInfo);
-    craigslistPostInfo.url = window.location.href;
-    craigslistPostInfo.id = window.location.href.match(/\d+\.html/i)[0].replace(/\.html/i, '');
-    return craigslistPostInfo;
+    return bedBathArray;
   }
-}
 
-function assembleSpreadsheetRowText(craigslistPostInfo) {
-  return craigslistPostInfo.price + "\t" + craigslistPostInfo.title + "\t" + craigslistPostInfo.address + "\t" + craigslistPostInfo.numBedrooms + "\t" + craigslistPostInfo.numBathrooms + "\t" + craigslistPostInfo.squareFootage + "\t" + craigslistPostInfo.parking + "\t" + craigslistPostInfo.housingType + "\t" + craigslistPostInfo.url + "\t" + craigslistPostInfo.id;
+  getNumBedrooms(bedBathArray) {
+    const bedroomsString = bedBathArray[0];
+    const bedRegex = /\d+BR/i;
+    if (bedRegex.test(bedroomsString)) {
+      this.numBedrooms = parseInt(bedroomsString.match(/\d+/)[0]);
+    }
+  }
+
+  getNumBathrooms(bedBathArray) {
+    const bathRoomsString = bedBathArray[1];
+    const bathRegex = /\d+BA/i;
+    if (bathRegex.test(bathRoomsString)) {
+      this.numBathrooms = parseInt(bathRoomsString.match(/\d+/)[0]);
+    }
+  }
+
+  getAvailableDate(firstRow) {
+    if (!(firstRow && firstRow.length && firstRow.length >= 3 && firstRow[2])) { // TODO: Make this clearer?
+      console.log('No available date listed - skipping');
+      return;
+    }
+    const availableDateFullString = firstRow[2].textContent;
+    this.availableDate = availableDateFullString.split('available ')[1]; // E.g. 'oct 1'
+  }
+
+  // Lack of IDs means we just have to iterate over the relevant spans
+  // and pick out relevant info as it pops up
+  getSecondRowInfo() {
+    const sections = [].slice.call(document.getElementsByClassName('attrgroup'));
+    sections.forEach(section => {
+      const spans = [].slice.call(section.children).filter(el => el.tagName === 'SPAN');
+      spans.forEach(span => {
+        if (/parking/i.test(span.innerText) || /garage/i.test(span.innerText) || /carport/i.test(span.innerText)) {
+          this.parking = span.innerText;
+        }
+        else if (/house/i.test(span.innerText) || /apartment/i.test(span.innerText) || /condo/i.test(span.innerText) || /duplex/i.test(span.innerText)) {
+          this.housingType = span.innerText;
+        }
+        else if (/w\/d/i.test(span.innerText) || /laundry/i.test(span.innerText)) {
+          this.washerDryer = span.innerText;
+        }
+        else if (/furnished/i.test(span.innerText)) {
+          this.furnished = 'Yes';
+        }
+      });
+    });
+  }
+
+  // Title, price, and square footage
+  getTitleInfo() {
+    const titleArray = document.querySelectorAll('h2.postingtitle');
+    if (titleArray.length && titleArray.length !== 1) {
+      const errorMessage = 'Something went wrong while trying to get the title :(';
+      alert(errorMessage);
+      throw new Error(errorMessage);
+    }
+    const title = titleArray[0];
+    // TODO: Add some validation/checks here
+    const titleSpan = [].slice.call(document.querySelectorAll('h2.postingtitle')[0].children).filter(child => child.className === 'postingtitletext')[0];
+    const titleChildren = [].slice.call(titleSpan.children);
+    this.price = parseInt(titleChildren.filter(child => child.className === 'price')[0].innerText.replace('$',''));
+    // TODO: validation
+    this.squareFootage = document.querySelectorAll('span.housing')[0].innerText.match(/\d+ft2/i)[0].match(/\d+/)[0];
+    this.title = document.getElementById('titletextonly').innerText;
+  }
+
+  getAddress() {
+    const addressArray = document.querySelectorAll('div.mapaddress');
+    if (addressArray && addressArray.length && addressArray.length === 1) {
+      this.address = addressArray[0].innerText;
+    }
+  }
+
+  getUrl() {
+    this.url = window.location.href; 
+  }
+
+  getCraigslistPostId() {
+    this.craigslistPostId = window.location.href.match(/\d+\.html/i)[0].replace(/\.html/i, '');
+  }
+
+
+  // Usage: copy the returned result and paste it into a cell in a spreadsheet
+  assembleSpreadsheetRowText() {
+    return this.price + "\t" + this.title + "\t" + this.address + 
+      "\t" + this.numBedrooms + "\t" + this.numBathrooms + "\t" + 
+      this.squareFootage + "\t" + this.parking + "\t" + 
+      this.housingType + "\t" + this.url + "\t" + this.craigslistPostId;
+  }
 }
 
 chrome.runtime.onMessage.addListener((request, sender) => {
   switch(request.message) {
     case 'GET CL INFO':
-      chrome.runtime.sendMessage({ message: 'CL POST INFO', craigslistPostInfo: getAllCraigslistPostInfo() });
-      const allCraigslistPostInfo = getAllCraigslistPostInfo();
-      const spreadsheetRowText = assembleSpreadsheetRowText(allCraigslistPostInfo);
+      const allCraigslistPostInfo = new CraigslistPostInfo();
+      const spreadsheetRowText = allCraigslistPostInfo.assembleSpreadsheetRowText();
       alert(spreadsheetRowText);
       break;
     default:
